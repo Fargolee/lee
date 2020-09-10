e3d40af29d1c00777093bd360b5804413397be1d
lee <775977072@qq.com>
2020-09-10 10:02:40 +0800

    1

--- 1.py
+++ 1.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 import time 
-
+"""
+练习selenium
+"""
 driver = webdriver.Chrome()
 driver.get("http://192.168.0.249:18080/common")
 driver.find_element_by_id("username").send_keys('15839900966')
 driver.find_element_by_id("password").send_keys('qq.123')
 driver.find_element_by_id("login").click()
-driver.implicitly_wait(2)
+driver.implicitly_wait(1)
 
 driver.find_element_by_xpath('//*[@id="search"]').send_keys('410104199409091621')
 driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[2]/div[2]/img').click()
